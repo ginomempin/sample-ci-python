4082c3134117a172bb046bdafef10badad86784b
gino <gino@alink-group.co.jp>
2019-03-28 19:26:02 +0900

    Check if coverage is really working

--- tests/provider_tests/mynumbers_test.py
+++ tests/provider_tests/mynumbers_test.py
@@ -2,19 +2,19 @@
 from app.provider import mynumbers
 
 
-def test_generate_integers_length():
-    assert len(mynumbers.generate_integers(1, 10, 2)) == 5
+# def test_generate_integers_length():
+#     assert len(mynumbers.generate_integers(1, 10, 2)) == 5
 
 
-def test_generate_integers_start_and_stop():
-    nums = mynumbers.generate_integers(1, 10, 2)
-    assert 1 in nums
-    assert 10 not in nums
+# def test_generate_integers_start_and_stop():
+#     nums = mynumbers.generate_integers(1, 10, 2)
+#     assert 1 in nums
+#     assert 10 not in nums
 
 
-def test_generate_integers_step():
-    nums = mynumbers.generate_integers(1, 10, 2)
-    assert all(n in nums for n in (1, 3, 5, 7, 9))
+# def test_generate_integers_step():
+#     nums = mynumbers.generate_integers(1, 10, 2)
+#     assert all(n in nums for n in (1, 3, 5, 7, 9))
 
 
 # def test_generate_integers_fail():
