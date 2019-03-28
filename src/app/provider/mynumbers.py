95dc70a0711a03e5a82fee4303f5c2e59abc243a
gino <gino@alink-group.co.jp>
2019-03-28 19:33:33 +0900

    Fix code coverage

--- src/app/provider/mynumbers.py
+++ src/app/provider/mynumbers.py
@@ -5,5 +5,7 @@ import numpy
 def generate_integers(start, stop, step):
     return numpy.arange(start, stop, step, dtype=int)
 
+
+# This is for checking that code coverage will be <100%.
 def generate_something():
     return True
