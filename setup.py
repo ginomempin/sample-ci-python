b99264d5fec3abcf42a0c07226fa0a378f8f7417
gino <gino.mempin@gmail.com>
2020-08-01 15:31:23 +0900

    Update for Python3.8 and pytest 6.x

--- setup.py
+++ setup.py
@@ -3,14 +3,9 @@ from setuptools import setup, find_namespace_packages
 setup(
     name="myapp",
     version="0.0.1",
-    package_dir={"" : "src"},
+    package_dir={"": "src"},
     packages=find_namespace_packages(where="src"),
-    python_requires='>=3.5',
-    install_requires=[
-        "numpy"
-    ],
-    test_requires=[
-        "pytest",
-        "pytest-cov"
-    ]
+    python_requires=">=3.8",
+    install_requires=["numpy"],
+    test_requires=["pytest", "pytest-cov"],
 )
